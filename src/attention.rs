cf591a2a5c9d5391c780b3d430552eec41de2337
hlhr202 <hlhr202@163.com>
2023-05-31 18:14:34 +0800

    update: optimize copy

--- src/attention.rs
+++ src/attention.rs
@@ -135,7 +135,7 @@ impl Attention {
             None => (key, value),
         };
 
-        let present = Tensor::stack(&[key.transpose(-2, -1), value.copy()], 0);
+        let present = Tensor::stack(&[key.transpose(-2, -1), value.shallow_clone()], 0);
         let a = self.attention(&query, &key, &value, attention_mask);
         let a = self.flatten(&a).apply(&self.c_proj);
 
